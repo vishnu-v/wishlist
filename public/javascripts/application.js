bdebcb35e9d7af06752cb716997589559481a21b
harikrishnan <harikrishnan.a@infibeam.net>
2015-08-30 06:34:29 +0530

    UX: js & styling fixes

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,15 @@
 $(function(){
   $(document)
-  .on('click', '.product-wrapper > .action > .contribute', function(){
+  .on('mouseenter', '.product ', function(){
+    $(this).find('.card-image .activator').trigger('click');
+  })
+  .on('mouseleave', '.product ', function(){
+    $(this).find('.card-title').trigger('click');
+  })
+  .on('click', '.product  .actions > .contribute', function(){
     confirm('Sure to contribute $1000?');
   })
-  .on('click', '.action > .buy', function(){
+  .on('click', '.product  .actions > .buy', function(){
     alert('Item bought');
   });
   $('.button-collapse').sideNav({menuWidth: 200});
